b58d86a63f061f9343a84e49ec6642366cc75b25
Armenian1 <avsenian97@gmail.com>
2022-07-08 18:47:05 -0400

    Add metro.config.js comment

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,7 @@ const { getDefaultConfig } = require("@expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+// Causes files with a .cjs extension to be considered an asset.
 defaultConfig.resolver.assetExts.push("cjs");
 
 module.exports = defaultConfig;
