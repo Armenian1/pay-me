47d82df252faa1c93e54c5d0fd4a76403ad7defb
Armenian1 <avsenian97@gmail.com>
2022-07-17 20:48:11 -0400

    Add theme to Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,45 +1,48 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { withTheme, Text } from 'react-native-paper';
 import RNModal from 'react-native-modal';
 
+const makeStyles = (theme: ReactNativePaper.Theme) =>
+   StyleSheet.create({
+      container: {
+         backgroundColor: theme.colors.surface,
+         borderRadius: 25,
+         borderWidth: 1,
+         borderColor: '#000',
+         borderStyle: 'solid',
+      },
+      header: {
+         alignItems: 'center',
+         justifyContent: 'center',
+      },
+      text: {
+         paddingTop: 10,
+         textAlign: 'center',
+         fontSize: 24,
+      },
+      body: {
+         justifyContent: 'center',
+         paddingHorizontal: 15,
+         paddingVertical: 15,
+         minHeight: 100,
+      },
+      footer: {
+         justifyContent: 'center',
+         alignItems: 'center',
+         padding: 10,
+         flexDirection: 'row',
+      },
+   });
+
 type ModalProps = {
+   theme: ReactNativePaper.Theme;
    isVisible: boolean;
    children: React.ReactNode;
    onBackdropPress: () => void;
 };
 
-const styles = StyleSheet.create({
-   container: {
-      backgroundColor: '#f5f5f5',
-      borderRadius: 25,
-      borderWidth: 1,
-      borderColor: '#000',
-      borderStyle: 'solid',
-   },
-   header: {
-      alignItems: 'center',
-      justifyContent: 'center',
-   },
-   text: {
-      paddingTop: 10,
-      textAlign: 'center',
-      fontSize: 24,
-   },
-   body: {
-      justifyContent: 'center',
-      paddingHorizontal: 15,
-      paddingVertical: 15,
-      minHeight: 100,
-   },
-   footer: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 10,
-      flexDirection: 'row',
-   },
-});
-
-const Modal = ({ isVisible = false, children, onBackdropPress, ...props }: ModalProps) => {
+const Modal = ({ theme, isVisible = false, children, onBackdropPress, ...props }: ModalProps) => {
    return (
       <RNModal
          isVisible={isVisible}
@@ -56,27 +59,42 @@ const Modal = ({ isVisible = false, children, onBackdropPress, ...props }: Modal
    );
 };
 
-const ModalContainer = ({ children }: { children: React.ReactNode }) => (
-   <View style={styles.container}>{children}</View>
-);
+const ModalContainer = ({
+   theme,
+   children,
+}: {
+   theme: ReactNativePaper.Theme;
+   children: React.ReactNode;
+}) => <View style={makeStyles(theme).container}>{children}</View>;
 
-const ModalHeader = ({ title }: { title: string }) => (
-   <View style={styles.header}>
-      <Text style={styles.text}>{title}</Text>
-   </View>
-);
+const ModalHeader = ({ theme, title }: { theme: ReactNativePaper.Theme; title: string }) => {
+   const styles = makeStyles(theme);
+   return (
+      <View style={styles.header}>
+         <Text style={styles.text}>{title}</Text>
+      </View>
+   );
+};
 
-const ModalBody = ({ children }: { children?: React.ReactNode }) => (
-   <View style={styles.body}>{children}</View>
-);
+const ModalBody = ({
+   theme,
+   children,
+}: {
+   theme: ReactNativePaper.Theme;
+   children?: React.ReactNode;
+}) => <View style={makeStyles(theme).body}>{children}</View>;
 
-const ModalFooter = ({ children }: { children?: React.ReactNode }) => (
-   <View style={styles.footer}>{children}</View>
-);
+const ModalFooter = ({
+   theme,
+   children,
+}: {
+   theme: ReactNativePaper.Theme;
+   children?: React.ReactNode;
+}) => <View style={makeStyles(theme).footer}>{children}</View>;
 
-Modal.Header = ModalHeader;
-Modal.Container = ModalContainer;
-Modal.Body = ModalBody;
-Modal.Footer = ModalFooter;
+Modal.Header = withTheme(ModalHeader);
+Modal.Container = withTheme(ModalContainer);
+Modal.Body = withTheme(ModalBody);
+Modal.Footer = withTheme(ModalFooter);
 
-export default Modal;
+export default withTheme(Modal);
